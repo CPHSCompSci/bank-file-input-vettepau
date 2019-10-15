dcf2cf938363c64e3ee43e4934370bbb25258e1f
vettepau <56130366+vettepau@users.noreply.github.com>
2019-10-14 20:46:38 -0500

    bank 1.1
    completed the user interface however the loadAccounts method isn't opperating correctly

--- src/app/Bank.java
+++ src/app/Bank.java
@@ -1,10 +1,12 @@
 package app;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
 	// Variable for logging/not logging
-	private static final boolean LOG = true;
+	private static final boolean LOG = false;
 
 	private static int accountCounter = 1;
 	private String name;
@@ -24,67 +26,129 @@ public class Bank {
 		Account newAccount = new Account(name);
 		accounts.add(newAccount);
 
-		log("Added account " + newAccount);
+		System.out.println("Successful Transaction");
 		return newAccount.accountNumber;
 	}
 
 	public boolean closeAccount(int accountNumber) {
 		Account account = findAccount(accountNumber);
 		if (account == null) {
-			log("Could not close account " + accountNumber);
+			System.out.println("Could not close account, please check if that was the right number");
+			Main.example1();
 			return false;
 		}
 		accounts.remove(account);
-		log("Successfully closed " + account);
+		System.out.println("Successfully closed, hopefully our service has been satisfactory");
 		return true;
 	}
 
 	public boolean deposit(int accountNumber, int amount) {
 		Account account = findAccount(accountNumber);
 		if (account == null) {
-			log("Could not deposit to account " + accountNumber);
+			System.out.println("Could not deposit to account , please check if that was the right number");
 			return false;
 		}
 		account.balance += amount;
-		log("Successfully deposited $" + amount + " to " + account);
+		System.out.println("Successfully deposited $" + amount + " to your account");
 		return true;
 	}
 
 	public boolean withdraw(int accountNumber, int amount) {
 		Account account = findAccount(accountNumber);
 		if (account == null) {
-			log("Could not withdraw from account " + accountNumber);
+			System.out.println("Could not withdraw from account, please check if that was the right number ");
 			return false;
 		}
 		if (account.balance < amount) {
-			log("Insufficient funds in " + account);
+			System.out.println("Insufficient funds in ");
 			return false;
 		}
 		account.balance -= amount;
-		log("Successfully withdrew $" + amount + " from " + account);
+		System.out.println("Successfully withdrew $" + amount);
 		return true;
 	}
 
 	public int checkBalance(int accountNumber) {
 		Account account = findAccount(accountNumber);
 		if (account == null) {
-			log("Could not check balance of account " + accountNumber);
+			System.out.println("Could not check balance of account, please check if that was the right number");
 			return -1;
 		}
-		log("Successfully checked balance of account " + account);
+		System.out.println("Your balance is: " + account.balance);
 		return account.balance;
 	}
 
-	public void saveAccounts(String filename) {
-		// TODO
-		log("Save not yet implemented.");
+	public void transferFunds(int accountnum, int accountnum2, int amount) {
+		Account account = findAccount(accountnum);
+		Account account2 = findAccount(accountnum2);
+		if (account.equals(null) && account2.equals(null))
+		{
+			System.out.println("Could not transfer funds, please check if that was the right number");
+		}
+		else {
+			account2.balance += amount;
+			System.out.println("Successfully transfered $" + amount);
+		}
+		
+	}
+	
+	public static void saveAccounts(String filename) {
+		try {
+			FileWriter fw = new FileWriter ("bankingDataBase");
+			fw.append(filename);
+			fw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		//log("Save not yet implemented.");
 	}
 
-	public void loadAccounts(String filename) {
-		// TODO
-		log("Load not yet implemented.");
+	public static void loadAccounts(String filename) {
+		try {
+			File inputfile = new File("info");
+			Scanner fileIn = new Scanner(inputfile);
+			
+			String line1 = fileIn.nextLine();
+			//String[] nums = line1.split(" ");
+			
+			String word = null;
+			int wordCount = indexRepeat(' ', filename) + 1;		
+			for (int a = 0; a < wordCount; a++)
+			{
+				int pos = filename.indexOf(' ');
+				if(filename.indexOf(' ') == -1)			
+				{
+					String Account = filename;
+					//Account a = new Account(a,)
+					
+				}
+				else {
+				String account = filename.substring(0, pos);
+				} 
+			}
+			fileIn.close();
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
+		//log("Load not yet implemented.");
 	}
 
+	public static int indexRepeat(char letter, String word)		
+	  {
+		  int a;
+		  int num = 0;
+		  for(a = 0; a < word.length()-1; a++)
+		  {
+			  if(word.charAt(a) == letter)
+			  {
+				  num++;
+			  }
+		  }	  
+		return num;
+	  }
+	
 	private Account findAccount(int accountNumber) {
 		for (int i = accounts.size() - 1; i >= 0; i--) {
 			if (accounts.get(i).accountNumber == accountNumber)
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,29 +1,105 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
 		example1();
+		again();
+	}
+	
+	public static void again() {
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Would you like to preform any additional banking functions");
+		System.out.println("yes or no");
+		String again = scan1.next();
+		if(again.equals("yes")) {
+			example1();
+			again();
+		}
+		else {}
 	}
 	
 	public static void example1()
 	{
-		Bank bank = new Bank("Bank of CPHS");
-		int ewbankAccountNo = bank.createAccount("Mr. Ewbank");
-		int howardAccountNo = bank.createAccount("Mr. Howard");
+		Bank bank = new Bank("la banca superiore di paul");
+		String files = "bankingDataBase";
+		Bank.loadAccounts(files);
+		Scanner scan = new Scanner(System.in);
 		
-		bank.deposit(ewbankAccountNo, 25);
-		bank.deposit(howardAccountNo, 75);
-
-		bank.checkBalance(ewbankAccountNo);
-		bank.checkBalance(howardAccountNo);
+		System.out.println("....... MENU ....... ");
+		System.out.println();
+		System.out.println("Deposit				1");
+		System.out.println("Withdraw			2");
+		System.out.println("Check Balance 			3");
+		System.out.println("Create Account			4");
+		System.out.println("Close Account 			5");
+		System.out.println("Transfer Funds 			6");
+		System.out.println();
+		System.out.println();
 		
-		bank.withdraw(ewbankAccountNo, 10);
-		bank.closeAccount(howardAccountNo);
+		int menu = scan.nextInt();
 		
-		bank.checkBalance(ewbankAccountNo);
-		bank.checkBalance(howardAccountNo);
+		switch(menu)
+		{
+		case 1: //deposit
+			System.out.println("Please enter your account number");
+			System.out.println();
+			int d1 = scan.nextInt();
+			System.out.println("Please enter the amount you would like to deposit");
+			System.out.println();
+			int d2 = scan.nextInt();
+			bank.deposit(d1, d2);
+		break;
 		
+		case 2: //withdraw
+			System.out.println("Please enter your account number");
+			System.out.println();
+			int w1 = scan.nextInt();
+			System.out.println("Please enter the amount you would like to withdraw");
+			System.out.println();
+			int w2 = scan.nextInt();
+			bank.withdraw(w1, w2);
+		break;
+		
+		case 3: //check balance
+			System.out.println("Please enter your account number");
+			System.out.println();
+			int c1 = scan.nextInt();
+			System.out.println(bank.checkBalance(c1));
+		break;
+		
+		case 4: //create account
+			System.out.println("Please enter your first and last name");
+			System.out.println();
+			String cta1 = scan.next();
+			System.out.println("Your new account number is: " + bank.createAccount(cta1));
+			System.out.println("Please save this number for future transactions");
+			System.out.println();
+		break;
+		
+		case 5: //close account
+			System.out.println("Please enter your account number");
+			System.out.println();
+			int cla1 = scan.nextInt();
+			bank.closeAccount(cla1);
+		break;
+		
+		case 6: //transfer funds
+			System.out.println("Please enter your account number");
+			System.out.println();
+			int tf1 = scan.nextInt();
+			System.out.println("Please enter the account number of the account you would like to transfer money");
+			System.out.println();
+			int tf2 = scan.nextInt();
+			System.out.println("Please enter the amount you would like to transfer");
+			System.out.println();
+			int tf3 = scan.nextInt();
+			bank.transferFunds(tf1, tf2, tf3);
+		break;
+		}
+		Bank.saveAccounts(files);
 	}
 }
